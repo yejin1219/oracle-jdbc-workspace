83a3863a769a7c36bde67df9e4cb31744c9eaf79
yejin1219 <133620338+yejin1219@users.noreply.github.com>
2023-06-26 23:12:01 +0900

    oracle\SQL02_SELECT(Function).sql 풀이 추가

--- oracle/SQL02_SELECT(Function).sql
+++ oracle/SQL02_SELECT(Function).sql
@@ -47,12 +47,34 @@ SELECT COUNT(*)
 FROM TB_STUDENT
 WHERE COACH_PROFESSOR_NO IS NULL;
 
---12.
-SELECT ;
+--12.학번이 A112113인 김고운 학생의 년도 별 평점을 구하는 SQL 문을 작성하시오. 단, 이때 출
+--력 화면의 헤더는 “년도”, “년도 별 평점” 이라고 찍히게 하고, 점수는 반올림하여 소수점 이하 한
+--자리까지만 표시한다.
+SELECT SUBSTR(TERM_NO,1,4) "년도", ROUND(AVG(POINT),1) "년도 별 평점"
+FROM TB_GRADE 
+WHERE STUDENT_NO = 'A112113'
+GROUP BY SUBSTR(TERM_NO,1,4)
+ORDER BY 1;
 
---13. null값 조심 
-SELECT  DEPARTMENT_NO "학과코드 명", COUNT(ABSENCE_NO) "휴학생 수"
+--13.학과 별 휴학생 수를 파악하고자 한다. 학과 번호와 휴학생 수를 표시하는 SQL 문장을 작성하시오
+SELECT  DEPARTMENT_NO "학과코드 명", COUNT(ABSENCE_YN) "휴학생 수"
 FROM TB_STUDENT
-GROUP BY DEPARTMENT_NO
-HAVING ABSENCE_NO = 'Y';
+GROUP BY DEPARTMENT_NO, ABSENCE_YN
+HAVING ABSENCE_YN = 'Y'
+ORDER BY 1;
+
+--14.춘 대학교에 다니는 동명이인 학생들의 이름을 찾고자 한다. 어떤 SQL 문장을 사용하면 가능
+--하겠는가 -- 풀기
+SELECT STUDENT_NAME "동명이인", COUNT(STUDENT_NAME) "동명인 수"
+FROM TB_STUDENT;
+
+--15.학번이 A112113 인 김고운 학생의 년도, 학기 별 평점과 년도 별 누적 평점, 총 평점을 구하
+--  는 SQL 문을 작성하시오. (단, 평점은 소수점 1 자리까지만 반올림하여 표시한다.
+SELECT SUBSTR(TERM_NO,1,4) "년도", SUBSTR(TERM_NO,5,2) "학기", ROUND(AVG(POINT),1) "학기 별 평점"
+FROM TB_GRADE 
+WHERE STUDENT_NO = 'A112113'
+GROUP BY ROLLUP (SUBSTR(TERM_NO,1,4),SUBSTR(TERM_NO,5,2)); 
+
+
+
 
